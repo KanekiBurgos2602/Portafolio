0592364dc93d4e30128b637f7416e5e979ce7467
Lappland-the-Decadenza <kanekiburgos256@gmail.com>
2025-10-19 18:27:41 -0600

    Configuracion para despliegue de Portafolio en Github Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import RouterWrapper from './RouterWrapper.jsx';
+import RouterWrapper from './Routes.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
